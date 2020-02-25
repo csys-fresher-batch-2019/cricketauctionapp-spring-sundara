21627f71b173c1e07e10b59f9f1a34d5fd1b4d65
Sundaramoorthi R <sundara2803@gmail.com>
2020-02-25 12:07:12 +0530

    initialized

---
+++ src/test/java/com/chainsys/libraryapp/LibraryappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chainsys.libraryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
